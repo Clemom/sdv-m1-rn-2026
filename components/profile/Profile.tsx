2aded07dc9a6e90efa1398af8e297f78ea46b857
Pierre Segalen <psegalen@outlook.com>
2026-03-11 16:18:27 +0100

    Ajout des informations du device sur la page Profil

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,5 +1,32 @@
-import { Text } from "react-native";
+import * as Device from "expo-device";
+import { Dimensions, Platform, StyleSheet, Text, View } from "react-native";
 
 export default function Profile() {
-  return <Text>Section à faire : profil utilisateur</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>Mon Username</Text>
+      <Text>
+        OS : {Platform.OS} {Device.osVersion}{" "}
+        {Platform.select({ ios: "by Apple", android: "by Someone" })}
+      </Text>
+      <Text>
+        {Device.manufacturer} : {Device.modelName}
+      </Text>
+      <Text>
+        Résolution : {Dimensions.get("screen").width} x{" "}
+        {Dimensions.get("screen").height} ({Dimensions.get("screen").scale})
+      </Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    gap: 16,
+  },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
